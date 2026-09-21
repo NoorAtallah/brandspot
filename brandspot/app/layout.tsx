eacf5efb7cdb629955ed81b8df7d2720ab3d7c76
DigitalCore360 <nooratallah1234@gmail.com>
2026-09-21 21:28:14 +0300

    new edits

--- brandspot/app/layout.tsx
+++ brandspot/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist } from "next/font/google";
 import { Tajawal } from "next/font/google";
 import "./globals.css";
@@ -7,12 +8,40 @@ import Providers from "./providers";
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const tajawal = Tajawal({ variable: "--font-tajawal", subsets: ["arabic", "latin"], weight: ["400", "500", "700", "800"] });
 
+const META_PIXEL_ID = "2515076895585004";
+
 export const metadata: Metadata = { title: "brans.spot", description: "Original brands for women, men & kids — Jordan" };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="ar" dir="rtl" className={`${geistSans.variable} ${tajawal.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            className="hidden"
+            alt=""
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
         <Providers>{children}</Providers>
       </body>
     </html>
